86f4c043035ef85ac409a29bf879734496b231ab
Kazuki Negoro <negoro.kazuki@tis.co.jp>
2020-10-07 12:55:29 +0900

    ✨feat: Added an utility to create IDs for Actor

---
+++ src/main/scala/lerna/akka/entityreplication/util/ActorIds.scala
@@ -0,0 +1,20 @@
+package lerna.akka.entityreplication.util
+
+object ActorIds {
+
+  private[this] val delimiter = ":"
+
+  def actorName(urlEncodedElements: String*): String = validateAndCreate(urlEncodedElements: _*)
+
+  def persistenceId(urlEncodedElements: String*): String = validateAndCreate(urlEncodedElements: _*)
+
+  private[this] def validateAndCreate(urlEncodedElements: String*): String = {
+    val invalidElements = urlEncodedElements.zipWithIndex.filter { case (e, _) => e.contains(delimiter) }
+    // Not URL encoded values induce ID duplication
+    require(
+      invalidElements.isEmpty,
+      s"Not URL encoded value found: ${invalidElements.map { case (e, i) => s"(${i + 1}: $e)" }.mkString(", ")}",
+    )
+    String.join(delimiter, urlEncodedElements: _*)
+  }
+}
