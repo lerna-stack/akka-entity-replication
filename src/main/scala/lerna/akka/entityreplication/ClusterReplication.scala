629a449db1d66cce5f45e1575d83dfcf052e775b
Kazuki Negoro <negokaz@gmail.com>
2021-05-21 19:16:54 +0900

    🐞fix: Add ClusterReplicationGuardian to avoid region creation collisions

--- src/main/scala/lerna/akka/entityreplication/ClusterReplication.scala
+++ src/main/scala/lerna/akka/entityreplication/ClusterReplication.scala
@@ -1,8 +1,17 @@
 package lerna.akka.entityreplication
 
-import akka.actor.{ ActorRef, ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider, Props }
+import akka.actor.{ Actor, ActorRef, ExtendedActorSystem, Extension, ExtensionId, ExtensionIdProvider, Props, Status }
+import lerna.akka.entityreplication.ClusterReplication.EntityPropsProvider
 import lerna.akka.entityreplication.model.TypeName
 import lerna.akka.entityreplication.raft.eventsourced.{ CommitLogStore, ShardedCommitLogStore }
+import lerna.akka.entityreplication.util.ActorIds
+import akka.util.Timeout
+import akka.pattern.ask
+import lerna.akka.entityreplication.ClusterReplicationGuardian.Started
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.util.control.NonFatal
 
 object ClusterReplication extends ExtensionId[ClusterReplication] with ExtensionIdProvider {
 
@@ -10,14 +19,15 @@ object ClusterReplication extends ExtensionId[ClusterReplication] with Extension
 
   override def createExtension(system: ExtendedActorSystem): ClusterReplication = new ClusterReplication(system)
 
-  private val actorNamePrefix: String = "replicationRegion"
-
   private[entityreplication] type EntityPropsProvider = ReplicationActorContext => Props
 }
 
 class ClusterReplication private (system: ExtendedActorSystem) extends Extension {
 
   import ClusterReplication._
+
+  private[this] lazy val guardian: ActorRef =
+    system.systemActorOf(ClusterReplicationGuardian.props(), "clusterReplicationGuardian")
 
   def start(
       typeName: String,
@@ -36,18 +46,75 @@ class ClusterReplication private (system: ExtendedActorSystem) extends Extension
       extractEntityId: ReplicationRegion.ExtractEntityId,
       extractShardId: ReplicationRegion.ExtractShardId,
   ): ActorRef = {
-    val _typeName = TypeName.from(typeName)
 
-    val maybeCommitLogStore: Option[CommitLogStore] = {
-      // TODO: RMUの有効無効をconfigから指定
-      val enabled = true // FIXME: settings から取得する (typeName ごとに切り替えられる必要あり)
-      // TODO: テストのために差し替え出来るようにする
-      Option.when(enabled)(new ShardedCommitLogStore(_typeName, system))
-    }
+    implicit val timeout = Timeout(30.seconds)
 
-    system.systemActorOf(
-      ReplicationRegion.props(typeName, entityProps, settings, extractEntityId, extractShardId, maybeCommitLogStore),
-      s"$actorNamePrefix-$typeName",
+    val start = ClusterReplicationGuardian.Start(
+      typeName = typeName,
+      entityProps,
+      settings,
+      extractEntityId,
+      extractShardId,
     )
+
+    Await.result((guardian ? start).mapTo[Started], timeout.duration).regionRef
+  }
+}
+
+private[entityreplication] object ClusterReplicationGuardian {
+  def props(): Props = Props(new ClusterReplicationGuardian())
+
+  sealed trait Command
+
+  final case class Start(
+      typeName: String,
+      entityProps: EntityPropsProvider,
+      settings: ClusterReplicationSettings,
+      extractEntityId: ReplicationRegion.ExtractEntityId,
+      extractShardId: ReplicationRegion.ExtractShardId,
+  ) extends Command
+
+  final case class Started(regionRef: ActorRef)
+}
+
+private[entityreplication] class ClusterReplicationGuardian extends Actor {
+  import ClusterReplicationGuardian._
+
+  override def receive: Receive = {
+
+    case Start(typeName, entityProps, settings, extractEntityId, extractShardId) =>
+      try {
+        val _typeName  = TypeName.from(typeName)
+        val regionName = ActorIds.actorName("replicationRegion", _typeName.underlying)
+
+        val maybeCommitLogStore: Option[CommitLogStore] = {
+          // TODO: RMUの有効無効をconfigから指定
+          val enabled = true // FIXME: settings から取得する (typeName ごとに切り替えられる必要あり)
+          // TODO: テストのために差し替え出来るようにする
+          Option.when(enabled)(new ShardedCommitLogStore(_typeName, context.system))
+        }
+
+        val regionRef: ActorRef =
+          context.child(regionName) match {
+            case Some(ref) => ref
+            case None =>
+              context.actorOf(
+                ReplicationRegion.props(
+                  typeName,
+                  entityProps,
+                  settings,
+                  extractEntityId,
+                  extractShardId,
+                  maybeCommitLogStore,
+                ),
+                regionName,
+              )
+          }
+
+        sender() ! Started(regionRef)
+      } catch {
+        case NonFatal(ex) =>
+          sender() ! Status.Failure(ex)
+      }
   }
 }
