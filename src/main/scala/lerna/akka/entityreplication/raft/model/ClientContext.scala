24de22abbfe4d935f131eda80e808c887106b13b
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2022-07-14 12:50:37 +0900

    Add ClientContext.forward

--- src/main/scala/lerna/akka/entityreplication/raft/model/ClientContext.scala
+++ src/main/scala/lerna/akka/entityreplication/raft/model/ClientContext.scala
@@ -7,4 +7,14 @@ private[entityreplication] final case class ClientContext(
     ref: ActorRef,
     instanceId: Option[EntityInstanceId],
     originSender: Option[ActorRef],
-)
+) {
+
+  /** Sends the given `message` to the actor `ref`, including the sender `originSender`
+    *
+    * If `originSender` is `None`, [[ActorRef.noSender]] is included as the sender.
+    */
+  def forward(message: Any): Unit = {
+    ref.tell(message, originSender.getOrElse(ActorRef.noSender))
+  }
+
+}
